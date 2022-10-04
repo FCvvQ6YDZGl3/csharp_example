e9e577d7573363232285772dc6ba960fa4a11b4c
star52 <abramco@yandex.ru>
2022-10-04 21:15:12 +0700

    Перенес проект тестирования в отдельное решение

--- Generalizations[Chapter18]/Generalizations[Chapter18]/Program.cs
+++ Generalizations[Chapter18]/Generalizations[Chapter18]/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
-namespace Generalizations_Chapter18_
+namespace GenericsDemo
 {
+    class Gen<T>
+    {
+        T ob;
+
+        public Gen(T o)
+        {
+            ob = o;
+        }
+        public T GetOb()
+        {
+            return ob;
+        }
+        public void ShowType()
+        {
+            Console.WriteLine("К типу T относится " + typeof(T));
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Gen<int> iOb;
+
+            iOb = new Gen<int>(102);
+
+            iOb.ShowType();
+
+            int v = iOb.GetOb();
+            Console.WriteLine("Значение: " + v);
+
+            Console.WriteLine();
+
+            Gen<string> strOb = new Gen<string>("Обобщения повышают эффективность.");
+
+            strOb.ShowType();
+
+            string str = strOb.GetOb();
+            Console.WriteLine("Значение: " + str);
+
+            Console.ReadKey();
         }
     }
 }
