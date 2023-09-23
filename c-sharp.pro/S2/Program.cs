1682d9dfa3d65093e13eb2725af29fca7483086a
Попов Петр <popov.petr.viktorovich@gmail.com>
2023-09-23 17:27:21 +0700

    Сократил решение задач. Теперь просто свелись к вводу формул.

--- c-sharp.pro/S2/Program.cs
+++ c-sharp.pro/S2/Program.cs
@@ -1,28 +1,31 @@
-﻿using System;
+﻿//https://c-sharp.pro
+using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace S2
 {
-    delegate void RunProgrammExercise();
-    delegate void Calculate();
+    delegate void RunProgrammExercise(List<double> args);
 
     class Exercise
     {
-        private Calculate calculate;
-        private RunProgrammExercise runProgrammExercise;
-        List<double> args;
-        public Exercise(Calculate calc)
+        public RunProgrammExercise runProgrammExercise;
+        public List<string> inputText;
+        public List<double> args;
+        private void getArgs()
         {
-            
-            calculate = calc;
+            args = new List<double>();
+            foreach(string t in inputText)
+            {
+                Console.WriteLine("{0}:", t);
+                args.Add(getNumberAndCheckFromConsole());
+            }
         }
-    }
 
-    class Program
-    {
-        static double getNumberAndCheckFromConsole()
+        private double getNumberAndCheckFromConsole()
         {
             string answer;
             double number;
@@ -30,12 +33,22 @@ namespace S2
 
             if (!double.TryParse(answer, out number))
             {
-                Console.WriteLine( "Вы ввели не число!");
+                getArgs();
+                Console.WriteLine("Вы ввели не число!");
                 number = getNumberAndCheckFromConsole();
             }
             return number;
         }
 
+        public void run()
+        {
+            getArgs();
+            runProgrammExercise(args);
+        }
+    }
+
+    class Program
+    {
         static string getExerciseNumberAndCheckFromConsole()
         {
             string answer;
@@ -51,75 +64,126 @@ namespace S2
         static void Main(string[] args)
         {
             string exrecise_number;
-            exrecise_number = getExerciseNumberAndCheckFromConsole();
-            double xVar;
-            double yVar;
-            double aVar;
 
             Console.Clear();
 
+            Dictionary<string, Exercise> exercices = new Dictionary<string, Exercise>();
 
-            Dictionary<string, RunProgrammExercise> exercices = new Dictionary<string, RunProgrammExercise>();
-            
-            exercices.Add("S2.1", new RunProgrammExercise(() => {
-                xVar = 1.2;
-                yVar = 7 * Math.Pow(xVar, 2) + 3 * xVar + 6;
-                Console.WriteLine("y=7x^2+3x+6 при {0} равен {1}", xVar, yVar);
+            //Хороший паттерн как не надо использовать делегаты)
+            Exercise exr = new Exercise();
+            exr.inputText = new List<string>{ "Введите значение x"};
+            exr.runProgrammExercise +=
+                new RunProgrammExercise((a) =>
+                {
+                    Console.WriteLine("y=7x^2+3x+6 при {0} равен {1}", a[0], (7 * Math.Pow(a[0], 2) + 3 * a[0] + 6) );
+                });
+            exercices.Add("S2.1a", exr);
 
-                aVar = 3;
-                xVar = 12 * Math.Pow(aVar, 2) + 7 * aVar + 12;
-                Console.WriteLine("x=12a^2+7a+12 при {0} равен {1}", aVar, xVar);
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите значение y" };
+            exr.runProgrammExercise += new RunProgrammExercise( (a) => {
+                Console.WriteLine("x=12a^2+7a+12 при {0} равен {1}", a[0], (12 * Math.Pow(a[0], 2) + 7 * a[0] + 12));
             }
-            ));
-            exercices.Add("S2.2", new RunProgrammExercise(() =>
-            {
-                double sideSquare = 3;
-                Console.WriteLine("Периметр квадрата при стороне {0} равен {1}", sideSquare, sideSquare * 4);
-            }));
-            exercices.Add("S2.3", new RunProgrammExercise(() =>
-            {
-                double rad = 3;
-                Console.WriteLine("Диаметр окружности с радиусом {0} равен {1}", rad, rad * 2);
-            }));
-            exercices.Add("S2.4", new RunProgrammExercise(() =>
-            {
-                const double earthRadius = 6350;
-                double height = 200;
-            }));
-            exercices.Add("S2.5", new RunProgrammExercise(() =>
-            {
-                double cubeEdge = 9;
-                Console.WriteLine("Куб с длинной ребра {0} имеет объем {1} и площадь поверхности {2}", cubeEdge, Math.Pow(cubeEdge, 3)
-                    , Math.Pow(cubeEdge, 2));
-            }));
-            exercices.Add("S2.6", new RunProgrammExercise(() =>
-            {
-                double rad = 3;
-                Console.WriteLine("Окружность радиусом {0} имеет длину {1} и площадь {2}", rad, 2 * Math.PI * rad, Math.PI * Math.Pow(rad, 2));
-            }));
-            exercices.Add("S2.7", new RunProgrammExercise(() =>
-            {
-                xVar = getNumberAndCheckFromConsole();
-                yVar = getNumberAndCheckFromConsole();
-                Console.WriteLine("Среднее арифметическое чисел {0} и {1} равно {2}", xVar, yVar, (xVar + yVar) / 2);
-                Console.WriteLine("Среднее геометрическое чисел {0} и {1} равно {2}", xVar, yVar, Math.Sqrt(xVar * yVar));
-            }));
-            exercices.Add("S2.8", new RunProgrammExercise(() =>
-            {
-                double volume = getNumberAndCheckFromConsole();
-                double massa = getNumberAndCheckFromConsole();
-                Console.WriteLine("Плотность тела с объемом {0} и массой {1} равна {2}", volume, massa,  massa/volume);
-            }));
-            exercices.Add("S2.9", new RunProgrammExercise(() =>
-            {
-                double square = getNumberAndCheckFromConsole();
-                double numberOfPerson = getNumberAndCheckFromConsole();
-                Console.WriteLine("Плотность населения тела на территории площадью {0} с количеством человек {1} равна {2}", square, numberOfPerson,  square/numberOfPerson);
-            }));
+            );
+            exercices.Add("S2.1b", exr);
 
-            if(exercices.ContainsKey(exrecise_number))
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите длину стороны квадрата" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Периметр квадрата при стороне {0} равен {1}", a[0], a[0] * 4);
+            }
+            );
+            exercices.Add("S2.2", exr);
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите значение радиуса" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Диаметр окружности с радиусом {0} равен {1}", a[0], a[0] * 2);
+            }
+            );
+            exercices.Add("S2.3", exr);
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите высоту предмета над поверхностью земли" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Эта задача оказалась мне не под силу");
+            }
+            );
+            exercices.Add("S2.4", exr);
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите длину стороны куба" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Куб с длинной ребра {0} имеет объем {1} и площадь поверхности {2}", a[0], Math.Pow(a[0], 3)
+                    , Math.Pow(a[0], 2));
+            });
+            exercices.Add("S2.5", exr);
+
+
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите радиус окружности"};
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Окружность радиусом {0} имеет длину {1} и площадь {2}", a[0], 2 * Math.PI * a[0], Math.PI * Math.Pow(a[0], 2));
+            });
+            exercices.Add("S2.6", exr);
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите число", "Введите число" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Среднее арифметическое чисел {0} и {1} равно {2}", a[0], a[1], (a[0] + a[1]) / 2);
+                Console.WriteLine("Среднее геометрическое чисел {0} и {1} равно {2}", a[0], a[1], Math.Sqrt(a[0] * a[1]));
+            });
+            exercices.Add("S2.7", exr);
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите объем", "Введите массу" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Плотность тела с объемом {0} и массой {1} равна {2}", a[0], a[1], a[0] / a[1]);
+            });
+            exercices.Add("S2.8", exr);
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите количество людей", "Введите площадь страны" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("Плотность населения на территории площадью {0} с количеством человек {1} равна {2}", a[0], a[1], a[0] / a[1]);
+            });
+            exercices.Add("S2.9", exr);
+
+
+            exr = new Exercise();
+            exr.inputText = new List<string> { "Введите значение a", "Введите значение b" };
+            exr.runProgrammExercise += new RunProgrammExercise((a) => {
+                Console.WriteLine("{0}x+{1}=0, x = {2}", a[0], a[1], -a[1]/a[0]);
+            });
+            exercices.Add("S2.10", exr);
+
+            /*
+            exercices.Add("S2.10", new RunProgrammExercise(() =>
             {
-                exercices[exrecise_number]();
+                //double a = getNumberAndCheckFromConsole();
+            }));
+            */
+
+            foreach (var kvp in exercices)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+            exrecise_number = getExerciseNumberAndCheckFromConsole();
+
+            if (exercices.ContainsKey(exrecise_number))
+            {
+                try
+                {
+                    exercices[exrecise_number].run();
+                    throw new Exception("Ошибка");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Что-то пошло не так: {0}", e);
+                    //Console.WriteLine(e.StackTrace);
+                }
             }
             else
             {
